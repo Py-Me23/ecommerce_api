e19af0ecc512a1b5d4799129fa2e0d8a51ababfb
Py-Me23 <oforiafia1@gmail.com>
2025-09-02 22:42:28 +0000

    Using the get method for ecommerce api

---
+++ products.py
@@ -0,0 +1,23 @@
+products = [
+    {
+        "id": 1,
+        "name": "shoes",
+        "description": "black",
+        "price": "Ghc300.00",
+        "image": "url(...)",
+    },
+    {
+        "id": 2,
+        "name": "T-Shirt",
+        "description": "pink",
+        "price": "Ghc 350.00",
+        "image": "url(...)",
+    },
+    {
+        "id": 3,
+        "name": "Trousers",
+        "description": "brown",
+        "price": "Ghc 500.00",
+        "image": "url(...)",
+    },
+]
